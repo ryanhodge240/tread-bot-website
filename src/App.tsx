046b4c60902eb785b0005322dc9ff3ea7616613d
Ryan Hodge <ryanhodge240@gmail.com>
2023-02-14 19:15:48 -0500

    Started working on the websocket stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const PORT = 9898;
+const WS_URL = `ws://192.168.1.1:${PORT}/ws`
+const ws = new WebSocket(WS_URL)
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<div>
+				<button onClick={() => ws.send('Test Message')}>Test Communication</button>
+			</div>
+		</>
+	);
 }
 
 export default App;
---
+++ src/AppStyles.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export default class AppStyles {
+	static readonly AppContainer = styled.div`
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        height: 100%;
+	`
+}
