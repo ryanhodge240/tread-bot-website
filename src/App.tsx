20de73cb3e32a50cf16b9cea29a91a0559e315e6
Ryan Hodge <ryanhodge240@gmail.com>
2023-02-15 01:01:53 -0500

    Added in receiving messages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import Styles from "./AppStyles";
 
-const PORT = 9898;
+const PORT = 8989;
 const WS_URL = `wss://192.168.1.1:${PORT}/ws`
 const ws = new WebSocket(WS_URL)
 
-function App() {
+function App() {	
+	ws.onmessage = function (event) {
+		const json = JSON.parse(event.data);
+		console.log(`[message] Data received from server: ${json}`);
+		try {
+			if (json.event === "data") { console.log(json.data); }
+		} catch (err) {
+			// whatever you wish to do with the err
+		}
+	};
+	
 	return (
 		<Styles.AppContainer>
 			<button onClick={() => ws.send('Test Message')}>Test Communication</button>
