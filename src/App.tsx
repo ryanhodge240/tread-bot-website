74f1e4c48ac2cb0e0ac0b9a10c67987d98ce1b08
Ryan Hodge <ryanhodge240@gmail.com>
2023-02-15 00:48:20 -0500

    Centered button

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Styles from "./AppStyles";
 
 const PORT = 9898;
 const WS_URL = `wss://192.168.1.1:${PORT}/ws`
@@ -5,12 +6,9 @@ const ws = new WebSocket(WS_URL)
 
 function App() {
 	return (
-		<>
-			<div>
-				<button onClick={() => ws.send('Test Message')}>Test Communication</button>
-				<p>Test</p>
-			</div>
-		</>
+		<Styles.AppContainer>
+			<button onClick={() => ws.send('Test Message')}>Test Communication</button>
+		</Styles.AppContainer>
 	);
 }
 
